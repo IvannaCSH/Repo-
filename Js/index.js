a310342917f22b3d1bbd9cba152053a7e3207e70
faustino santander <ivanna.camila.santander@gmail.com>
2021-09-07 00:28:28 -0500

    Actualización 11

--- Js/index.js
+++ Js/index.js
@@ -1,17 +1,27 @@
-const url = "https://raw.githubusercontent.com/IvannaCSH/Repo-Fundamentos-de-programacion-web/main/Js/datos.json";
+const span = document.getElementById("energía")
+const url = 'https://raw.githubusercontent.com/IvannaCSH/Repo-Fundamentos-de-programacion-web/main/Js/datos.json';
+let energía ="Energía Hidraúlica";
+fetch(url + energía)
+.then(function(response){
+return response.json();
+})
+.then(function(data){
+let p = crearNodo ("p");
+ let energía = crearNodo("span");
+ let id = crearNodo("span");
+ energía.innerHTML = data.energía;
+ id.innerHTML = data.id + "-";
+ adjuntar(p, id);
+ adjuntar(p, energía);
+ adjuntar(div, p);
 
-fetch(url).then( (response)=> {
-    return response.json();
-}).then( (data)=> {
-    energía = document.getElementById("energía");
-    energía.innerHTML = data.energía
-
-    porcentaje_mexico = document.getElementById('porcentaje_mexico');
-    porcentaje_mexico.innerHTML = data.porcentaje_mexico
-
-    porcentaje_mundo = document.getElementById('porcentaje_mundo');
-    porcentaje_mundo.innerHTML = data.porcentaje_mundo;
-
-}).catch(function (error) {
-    alert("Sucedió un error")
+})
+.catch(function (error) {
+ console.log(error);
 });
+function crearNodo(elemento){
+    return document.createElement(elemento);
+}
+function adjuntar(padre,hijo) {
+    return padre.appendChild(hijo);
+}
